745db1691bacf0cb24b310f6c4662a8307e517eb
e76nystrom <eric.nystrom@yahoo.com>
2023-12-20 07:10:53 -0500

    Changes for USBx.  Cube file updates.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -82,6 +82,7 @@ int main(void)
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
+/* Enable the CPU Cache */
 
   /* Enable I-Cache---------------------------------------------------------*/
   SCB_EnableICache();
